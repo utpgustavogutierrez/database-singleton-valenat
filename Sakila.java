da059784ba00409b73176c419492f4dd2328b577
valerie-alvarez <136520593+valerie-alvarez@users.noreply.github.com>
2024-04-06 23:23:42 +0000

    Singleton implementado

--- Sakila.java
+++ Sakila.java
@@ -5,11 +5,11 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Sakila
+
 {
-  public static void main(String[] args)
-  {
-    // NOTE: Connection and Statement are AutoClosable.
-    //       Don't forget to close them both in order to avoid leaks.
+  private static Sakila instance;
+
+  private Sakila() {     
     try
     (
       // create a database connection
@@ -28,9 +28,22 @@ public class Sakila
     }
     catch(SQLException e)
     {
-      // if the error message is "out of memory",
-      // it probably means no database file is found
       e.printStackTrace(System.err);
+      }   
     }
+
+   public static synchronized Sakila getInstance() {
+        if(instance == null) {
+            instance = new Sakila();
+        }
+        return instance;
+      }
+
+  public static void main(String[] args)
+  /**{
+   Sakila database = Sakila.getInstance();
+   System.out.println(database.Sakila());
   }
+  PREGUNTA: ¿Cómo se formulan las queries?**/
+
 }
